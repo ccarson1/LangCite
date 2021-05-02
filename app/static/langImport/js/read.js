9aa06e27b8a77c099ac22f9bc6068ef43817668d
Craig Carson <38870851+ccarson1@users.noreply.github.com>
2021-05-02 17:31:40 -0400

    lesson display update

--- app/static/langImport/js/read.js
+++ app/static/langImport/js/read.js
@@ -5,6 +5,7 @@ var csrf = $("input[name=csrfmiddlewaretoken]").val();
 var read_mode = getCookie("read_mode"); 
 var page = 0;
 var sentence_num;
+var read_page;
 
 if(read_mode == "sentence"){
 	sentence_num = 1;
@@ -44,15 +45,23 @@ function set_mode(mode){
 }
 
 function populate_words(){
-	if(store_json.up_method == "PDF" || store_json.up_method == "Text File" || store_json.up_method == "Image"){
+	position = getCookie("read_page");
+	if(position == ""){
+		position = 0;
+	}
+
+	
+
+	if(store_json.up_method == "PDF" || store_json.up_method == "Text File" || store_json.up_method == "Image" || store_json.up_method == "Youtube url"){
 		if(sentence_num > store_json.lesson_sentences.length ){
 			sentence_num = store_json.lesson_sentences.length;
 		}
 		if(store_json.target_lang == "Russian"){
 			for(i = 0; i < sentence_num; i++){
-				for(j=0; j < store_json.lesson_sentences[i].sentence_.length; j++){
-
-					createReadButtons(store_json.lesson_sentences[i].sentence_[j].Russian, j, i)
+				
+				for(j=0; j < store_json.lesson_sentences[position].sentence_.length; j++){
+					
+					createReadButtons(store_json.lesson_sentences[position].sentence_[j].Russian, j, i)
 				}
 				
 			}
@@ -62,9 +71,9 @@ function populate_words(){
 		if(store_json.target_lang == "French"){
 			for(i = 0; i < sentence_num; i++){
 				
-				for(j=0; j < store_json.lesson_sentences[i].sentence_.length; j++){
+				for(j=0; j < store_json.lesson_sentences[position].sentence_.length; j++){
 
-					createReadButtons(store_json.lesson_sentences[i].sentence_[j].French, j, i)
+					createReadButtons(store_json.lesson_sentences[position].sentence_[j].French, j, i)
 					
 				}		
 			}
@@ -72,9 +81,9 @@ function populate_words(){
 		if(store_json.target_lang == "Spanish"){
 			for(i = 0; i < sentence_num; i++){
 				// console.log(store_json.lesson_sentences[i].sentence_.length)
-				for(j=0; j < store_json.lesson_sentences[i].sentence_.length; j++){
+				for(j=0; j < store_json.lesson_sentences[position].sentence_.length; j++){
 
-					createReadButtons(store_json.lesson_sentences[page].sentence_[j].Spanish, j, i)
+					createReadButtons(store_json.lesson_sentences[position].sentence_[j].Spanish, j, i)
 					
 				}	
 			}
@@ -82,10 +91,11 @@ function populate_words(){
 
 		if(store_json.target_lang == "English"){
 			for(i = 0; i < sentence_num; i++){
+				
 				// console.log(store_json.lesson_sentences[i].sentence_.length)
-				for(j=0; j < store_json.lesson_sentences[i].sentence_.length; j++){
-
-					createReadButtons(store_json.lesson_sentences[i].sentence_[j].English, j, i)
+				for(j=0; j < store_json.lesson_sentences[position].sentence_.length; j++){
+					
+					createReadButtons(store_json.lesson_sentences[position].sentence_[j].English, j, i)
 				}
 			}
 		}
@@ -150,61 +160,128 @@ function createReadButtons(word, word_pos, sent_pos){
 		document.getElementById("json_lesson").appendChild(btn);
 		
 	}
-	
-}
 
-// sends the word to the server using ajax
-for(var i =0; i < document.getElementsByClassName("btn btn-light").length; i++){
-	document.getElementsByClassName("btn btn-light")[i].addEventListener("click", function(){
-		var new_word ='dddd'
-		btn_word = this.innerHTML;
-		if(store_json.up_method == "PDF" || store_json.up_method == "Text File" || store_json.up_method == "Image"){
-			
-			bust = this.id.split("_");
-			console.log("sentence " + bust[0] + " on word " + bust[1] + " = " + store_json.lesson_sentences[bust[0]].sentence_[bust[1]].English);
-			
-			document.getElementById("target_def").innerHTML = bust;
-		}else{
-			console.log("sentence " + bust[0] + " on word " + bust[1] + " = " );
-		}
-		document.getElementById("target_word").innerHTML = this.innerHTML;
-		
-		
-
-
-		$.ajax({
-			type: "POST",
-			url: path,
-			data: {
-				'btn_word': btn_word,
-				'btn_target': store_json.target_lang,
-				'btn_native': store_json.native_lang,
-				'csrfmiddlewaretoken': csrf
-			},
-			success: function(response){
-				console.log(response.native_word);
-				new_word = response.native_word;
-
-				var w = new Date();
-				w.setTime(w.getTime() + (1*24*60*60*1000));
-				var expires = "expires="+ w.toUTCString();
-				document.cookie = "trans_word=" + new_word + ";" + expires + ";path=/";
+	// sends the word to the server using ajax
+	for(var i =0; i < document.getElementsByClassName("btn btn-light").length; i++){
+		document.getElementsByClassName("btn btn-light")[i].addEventListener("click", function(){
+			var new_word ='dddd'
+			btn_word = this.innerHTML;
+			if(store_json.up_method == "PDF" || store_json.up_method == "Text File" || store_json.up_method == "Image" || store_json.up_method == "Youtube url"){
 				
+				bust = this.id.split("_");
+				// console.log("sentence " + bust[0] + " on word " + bust[1] + " = " + store_json.lesson_sentences[bust[0]].sentence_[bust[1]].English);
 				
-				
+				document.getElementById("target_def").innerHTML = bust;
+			}else{
+				console.log("sentence " + bust[0] + " on word " + bust[1] + " = " );
 			}
+			document.getElementById("target_word").innerHTML = this.innerHTML;
+			
+			
 
 
+			$.ajax({
+				type: "POST",
+				url: path,
+				data: {
+					'btn_word': btn_word,
+					'btn_target': store_json.target_lang,
+					'btn_native': store_json.native_lang,
+					'csrfmiddlewaretoken': csrf
+				},
+				success: function(response){
+					console.log(response.native_word);
+					new_word = response.native_word;
+
+					var w = new Date();
+					w.setTime(w.getTime() + (1*24*60*60*1000));
+					var expires = "expires="+ w.toUTCString();
+					document.cookie = "trans_word=" + new_word + ";" + expires + ";path=/";
+					
+					
+					
+				}
+
+
+			});
+			
+			new_word = getCookie("trans_word");
+			///set second parameter with the defition from server////
+			this.setAttribute("data-bs-content", new_word);
+			
+			return false;
 		});
 		
-		new_word = getCookie("trans_word");
-		///set second parameter with the defition from server////
-		this.setAttribute("data-bs-content", new_word);
-		
-		return false;
-	});
+	}
 	
 }
 
 
 
+//turns the pages andsets the page number as well as the progress bar
+//variables for turning pages
+var btn_left = document.getElementById("btn_left");
+var btn_right = document.getElementById("btn_right");
+var percent_bar = document.getElementById("read_bar");
+var container = document.getElementById("json_lesson");
+var percent = 0;
+
+var pageNum = Math.ceil(store_json.lesson_sentences.length/sentence_num); 
+var inc = 100/pageNum;
+
+
+btn_left.addEventListener("click", function(){
+	document.getElementById("json_lesson").innerHTML = "";
+	if(page == 0 || page < 0){
+		page = 0;
+	}
+	else{
+		page--;
+	}
+	percent = (inc * page)
+	
+	percent_bar.style.width =  percent + "%";
+	percent_bar.innerHTML = (Math.round(percent)) + "%";
+	console.log(sentence_num *page)
+	console.log("sentence_num = " + sentence_num)
+	console.log("pageNum = " + pageNum)
+
+	read_page = sentence_num * page;
+
+	var n = store_json.up_title;
+	var d = new Date();
+	d.setTime(d.getTime() + (1*24*60*60*1000));
+	var expires = "expires="+ d.toUTCString();
+	document.cookie ="read_page=" + read_page + ";" + expires + ";path=/";
+
+	populate_words();
+});
+
+btn_right.addEventListener("click", function(){
+	document.getElementById("json_lesson").innerHTML = "";
+	if(page == pageNum || page > pageNum){
+		page = pageNum;
+	}
+	else{
+		page++;
+	}
+	percent = (inc * page);
+	if(percent > 100){
+		percent = 100;
+	}
+	percent_bar.style.width =  percent + "%";
+	percent_bar.innerHTML = (Math.round(percent)) + "%";
+	console.log(sentence_num *page)
+	console.log("sentence_num = " + sentence_num)
+	console.log("pageNum = " + pageNum)
+
+	read_page = sentence_num * page;
+
+	var n = store_json.up_title;
+	var d = new Date();
+	d.setTime(d.getTime() + (1*24*60*60*1000));
+	var expires = "expires="+ d.toUTCString();
+	document.cookie = "read_page=" + read_page + ";" + expires + ";path=/";
+
+	populate_words();
+});
